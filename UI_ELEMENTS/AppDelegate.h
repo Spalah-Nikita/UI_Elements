cbca6377d23479e7f73816ec0d8137a0ed426ec0
Nikita Vintonovich <nikitavintonovich@MacBook-Pro-Nikita.local>
2017-09-02 15:58:14 +0300

    Project of 8th lrsson was added

---
+++ UI_ELEMENTS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI_ELEMENTS
+//
+//  Created by Nikita Vintonovich on 9/1/17.
+//  Copyright © 2017 Vint-Rock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UI_ELEMENTS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UI_ELEMENTS
+//
+//  Created by Nikita Vintonovich on 9/1/17.
+//  Copyright © 2017 Vint-Rock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
